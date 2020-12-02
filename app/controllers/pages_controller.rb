55c8b9c6b02dc9fbab807dc935305623142936af
superchen <chensuper0202@gmail.com>
2020-12-02 22:14:25 +0800

    show stories in index page(root)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
+    
     def index
+        @stories = Story.order(created_at: :desc).includes(:user)
+        #@stories = current_user.stories.order(created_at: :desc)
     end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
-  
+
+  root 'pages#index'
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users, controllers: {
    registrations: 'users/registrations'
   }
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-  root 'pages#index'
   resources :stories
 
-  #get '/demo', to: 'pages#demo'
+  get '@:username/:story_id', to: 'pages#show', as: 'story_page'
+  get '@:username', to: 'pages#user', as: 'user_page' 
 
+
+  #get '/demo', to: 'pages#demo'
 end
