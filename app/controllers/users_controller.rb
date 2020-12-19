18237d0c54d224f9e74267d64ec53fce2d534a5b
superchen <chensuper0202@gmail.com>
2020-12-19 22:57:41 +0800

    begore paypal SDK setting finished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
     
     def pricing
+
     end
 
     def payment
+        @fee = ENV["price_#{params[:type]}"]
+        @payment_type = (params[:type] == "vip") ? "VIP會員" : "白金會員"
     end
 
 end
